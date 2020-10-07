b40967cdd5a45516940930b5f7f2d565cc037553
anwesh43 <anweshthecool0@gmail.com>
2020-10-08 03:14:52 +0530

    adding util functions

---
+++ src/utils.js
@@ -0,0 +1,11 @@
+const maxScale = (scale, i, n) => Math.max(0, scale - i / n)
+
+export const divideScale = (scale, i, n) => Math.min(1 / n, maxScale(scale, i, n)) * n 
+
+export const sinify = (scale) => Math.sin(scale * Math.PI)
+
+export const resizeCb = (cb) => {
+    window.onresize = () => {
+        cb()
+    }
+}
